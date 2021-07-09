8ee472c59ac2d76e4ca3358dd28a2f6f8cbaf3a7
Pauline122112 <pauline.design007@gmail.com>
2021-07-09 10:36:22 -0500

    completed topics tab in Task 3

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,34 @@
 const Tabs = (topics) => {
+
+  const mainNav = document.createElement('div')
+  mainNav.classList.add(`topics`)
+  topics.forEach(element => {
+    const tabNav = document.createElement('div')
+    tabNav.classList.add('tab')
+    tabNav.textContent = element
+    mainNav.appendChild(tabNav)
+  })
+
+return mainNav
+
+}
+
+
+
+const tabsAppender = (selector) => {
+  // TASK 4
+  // ---------------------
+  // Implement this function which takes a css selector as its only argument.
+  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
+  // Find the array of topics inside the response, and create the tabs using the Tabs component.
+  // Append the tabs to the element in the DOM that matches the selector passed to the function.
+  //
+}
+
+export { Tabs, tabsAppender }
+
+
+
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -13,16 +43,3 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
-
-const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
-
-export { Tabs, tabsAppender }
